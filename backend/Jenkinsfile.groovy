12124410f5a20903ec4907edde8b6890c9a95c33
tntntnth7 <tntntnth@naver.com>
2023-07-29 12:39:29 +0900

    jenkins file commit5
    jenkins file commit5

--- backend/Jenkinsfile.groovy
+++ backend/Jenkinsfile.groovy
@@ -1,13 +1,13 @@
 pipeline {
     agent any
     environment {
-        DEPLOY_SERVER_ID = "ubuntu"
-        DEPLOY_SERVER_PASSWORD = "gjdbstj1!!"
-        SECRET_KEY = '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'
-        DB_USERNAME = 'ubuntu'
-        DB_PASSWORD = 'gjdbstj1!!'
-        DB_URL = 'web-db.cx1tqagnpe2l.ap-northeast-2.rds.amazonaws.com'
-        DB_PORT = '3306'
+        DEPLOY_SERVER_ID = credentials("ubuntu")
+        DEPLOY_SERVER_PASSWORD = credentials("gjdbstj1!!")
+        SECRET_KEY = credentials('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')
+        DB_USERNAME = credentials('ubuntu')
+        DB_PASSWORD = credentials('gjdbstj1!!')
+        DB_URL = credentials('web-db.cx1tqagnpe2l.ap-northeast-2.rds.amazonaws.com')
+        DB_PORT = credentials('3306')
     }
 
     stages {
@@ -27,8 +27,10 @@ pipeline {
 
         stage("Build") {
             steps {
-                sh "cd backend"
-                sh "./gradlew clean build"
+                dirs('backend'){
+                    sh "pwd"
+                    sh "./gradlew clean build"
+                }
             }
         }
         /*
